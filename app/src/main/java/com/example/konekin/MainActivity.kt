81dbde6a854ce3853261c37c176efb9ad608f168
EricoAli <Your_Email>
2025-11-20 10:38:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/konekin/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.konekin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.konekin.databinding.ActivityMainBinding
+import com.example.konekin.model.Users
+import com.example.konekin.network.ApiClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val client = ApiClient.getInstance()
+        val response = client.getAllUsers()
+        val userList = ArrayList<String>()
+        response.enqueue(object : Callback<Users> {
+            override fun onResponse(call: Call<Users>, response: Response<Users>) {
+                for (i in response.body()!!.data) {
+                    userList.add(i.employeeName)
+                }
+                val listAdapter = ArrayAdapter(
+                    this@MainActivity,
+                    android.R.layout.simple_list_item_1,
+                    userList
+                )
+                binding.lvNama.adapter = listAdapter
+            }
+            override fun onFailure(call: Call<Users>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "Koneksi error",
+                    Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+}
